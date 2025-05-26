3eb4afde7808e8d71946dd4ebf03878d4d17f815
votre_nom <laposta.enzo@gmail.com>
2025-05-26 14:16:34 +0200

    display countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import ListCard from "./components/ListCard";
 
 
 function App() {
@@ -10,36 +11,11 @@ function App() {
       .then(data => data.json())
       .then(data => {
         data.sort((a, b) => a.name.common.localeCompare(b.name.common))
-        console.log(data);
-
-        //   {
-        //   if (a.name.common < b.name.common) {
-        //     return -1
-        //   } else if (a.name.common > b.name.common) {
-        //     return 1
-        //   } else {
-        //     return 0
-        //   }
-        // })
         setCountries(data)
-
       }
-      )
-      .catch(err => console.log(err)
       )
 
   }, [])
-
-  // try {
-  //   const response = await fetch('https://restcountries.com/v3.1/region/europe')
-  //   const data = await response.json()
-
-  //   console.log(data);
-
-  // } catch (error) {
-  //   console.log(error);
-
-  // }
 
 
 
@@ -48,6 +24,15 @@ function App() {
       <div className="max-w-7xl mx-auto py-20 px-4">
         <h1 className="text-gray-50 text-4xl">Europe Countries Data</h1>
         <p className="text-gray-100 text-xl mb-8">Click on a card to reveal a country's information.</p>
+
+        {countries && (
+          <ul className="grid min-[450px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
+            {countries.map((country, index) => (
+              <ListCard key={index} country={country} />
+            ))}
+          </ul>
+        )}
+
       </div>
     </div>
   )
